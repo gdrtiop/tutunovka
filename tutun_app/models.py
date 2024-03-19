a128a5c9225a580d06f4dd8fbccad12ac2a5e8e3
Stepan <stepan@iMac-Stepan.local>
2024-03-19 22:37:44 +0300

    conflicts one more time resolved

--- tutun_app/models.py
+++ tutun_app/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
-#from django.contrib.postgres.fields import ArrayField
+
+# from django.contrib.postgres.fields import ArrayField
 
 
 User.add_to_class('tg_username', models.CharField(max_length=75, default=None, unique=True, null=True))
+
 
 class Dot(models.Model):
     class Meta:
@@ -18,20 +20,23 @@ class Dot(models.Model):
 class Route(models.Model):
     class Meta:
         db_table = "Routes"
+
     Name = models.CharField(max_length=125, default='Untitled')
     author = models.ForeignKey(to=User, on_delete=models.CASCADE)
     date_in = models.DateTimeField()
     date_out = models.DateTimeField()
     comment = models.CharField(max_length=700)
-    #baggage = ArrayField(models.CharField(max_length=20))
+    # baggage = ArrayField(models.CharField(max_length=20))
     baggage = models.CharField(max_length=3000)
     note = models.CharField(max_length=700)
     rate = models.IntegerField(default='-1')
     dots = models.ManyToManyField(to=Dot)
 
+
 class Route_for_list(models.Model):
     class Meta:
         db_table = "Routes_for_list"
+
     Name = models.CharField(max_length=125, default='Untitled')
     author = models.ForeignKey(to=User, on_delete=models.CASCADE)
     comment = models.CharField(max_length=700)
