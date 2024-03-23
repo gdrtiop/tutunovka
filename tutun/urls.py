0ea5e89c9a0848d2752ad03d0d5c280a291d2bad
Ваше Имя <you@example.com>
2024-03-23 19:49:20 +0300

    done

--- tutun/urls.py
+++ tutun/urls.py
@@ -33,6 +33,6 @@ urlpatterns = [
     path('profile/<str:stat>', views.profile, name='profile'),
     path('new_route', views.create_route, name='new_route'),
     path('route_detail/<int:route_id>/', views.route_detail, name='route_detail'),
-    path('public_routes', views.public_routes_page.as_view(), name='public_routes')
+    path('public_routes/', views.public_routes_page.as_view(), name='public_routes')
 
 ]
