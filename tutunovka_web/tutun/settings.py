4431004e27894c34a73acdbc8ce4796be1f792d5
Stepan Chibisov <fjjarm@gmail.com>
2024-04-20 00:01:25 +0300

    docker postgres & django done

--- tutunovka_web/tutun/settings.py
+++ tutunovka_web/tutun/settings.py
@@ -18,7 +18,7 @@ from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-load_dotenv()
+load_dotenv('.env.django')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
